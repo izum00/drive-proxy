e67c66d6e2c9851123fa77cf81774759399c677a
izum00 <karisute14@gmail.com>
2025-07-23 07:37:56 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-const stream = require('stream');
 
 const app = express();
-const PORT = 7860;
-
-app.use(cors()); // すべてのドメインを許可
+app.use(cors());
 
 app.get('/', async (req, res) => {
     const fileId = req.query.file_id;
@@ -16,22 +13,17 @@ app.get('/', async (req, res) => {
         return res.status(400).send('file_idパラメータが必要です');
     }
 
-    // 不要なクエリを除去
     const cleanFileId = fileId.split('?')[0];
     const url = `https://drive.google.com/uc?export=download&id=${cleanFileId}`;
 
     try {
-        const response = await axios.get(url, {
-            responseType: 'stream'
-        });
-
+        const response = await axios.get(url, { responseType: 'stream' });
         res.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
-        response.data.pipe(res); // ストリームとして送信
+        response.data.pipe(res);
     } catch (err) {
         res.status(err.response?.status || 500).send('ファイルの取得に失敗しました');
     }
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on http://0.0.0.0:${PORT}`);
-});
+// 必須：Vercelではこのようにモジュールとしてエクスポートする
+module.exports = app;
